4b78ccd3e399ec40235721094e168ce86e9db9e9
tajwarsaiyeed <rjabid36@gmail.com>
2024-06-14 14:58:07 +0600

    feat : meeting room setup | useGetCallById hook add for call by id
    -> start implementing Meeting room component

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+'use client'
+import React, {useState} from 'react';
+import {useUser} from "@clerk/nextjs";
+import {StreamCall, StreamTheme} from "@stream-io/video-react-sdk";
+import MeetingSetup from "@/app/(root)/meeting/[id]/components/meeting-setup";
+import MeetingRoom from "@/app/(root)/meeting/[id]/components/meeting-room";
+import {useGetCallById} from "@/hooks/useGetCallById";
+import Loader from "@/components/loader";
 
-const Meeting = ({ params }: { params: { id: string } }) => {
+const Meeting = ({params: {id}}: { params: { id: string } }) => {
+    const { isLoaded} = useUser();
+
+    const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+    const {call, isCallLoading} = useGetCallById(id);
+
+    if (!isLoaded || isCallLoading) return <Loader/>
+
     return (
-        <div>
-            Meeting Room: #{params.id}
-        </div>
+        <main className={'h-screen w-full'}>
+            <StreamCall call={call}>
+                <StreamTheme>
+                    {!isSetupComplete ? (
+                        <MeetingSetup setIsSetupComplete={setIsSetupComplete}/>
+                    ) : (
+                        <MeetingRoom/>
+                    )}
+                </StreamTheme>
+            </StreamCall>
+        </main>
     );
 };
 
